74d96f3872f19f6a8890dca2fc34630aadebd5ea
manumariuzza <81269138+manumariuzza@users.noreply.github.com>
2025-10-20 11:47:44 -0300

    Update reservationRoutes.ts

--- ru-platform-server/src/routes/reservationRoutes.ts
+++ ru-platform-server/src/routes/reservationRoutes.ts
@@ -8,9 +8,9 @@ const router = Router();
 router.post('/', authMiddleware, ReservationController.createReservation);
 
 router.get('/', authMiddleware, adminMiddleware, ReservationController.getReservations);
+router.get('/me', authMiddleware, ReservationController.getUserReservations);
 router.get('/day', authMiddleware, adminMiddleware, ReservationController.getTodayReservations);
 router.get('/week', authMiddleware, adminMiddleware, ReservationController.getWeekReservations);
-router.get('/me', authMiddleware, ReservationController.getUserReservations);
 
 router.patch('/:id', authMiddleware, ReservationController.updateReservation);
 router.patch('/status/:id', authMiddleware, adminMiddleware, ReservationController.updateReservationStatus);
